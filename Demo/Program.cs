af7257fc512cfae432fffa60f00b15de1f29d0c1
FenzDev <68622361+FenzDev@users.noreply.github.com>
2025-07-23 00:46:13 +0100

    Final touches.

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -12,14 +12,16 @@ namespace Msdfgen.ManualTest
             double advance = 0;
             var font = ImportFont.LoadFont("micross.ttf"); 
             var shape = ImportFont.LoadGlyph(font, 'A', ref advance, out int bitmapWidth, out int bitmapHeight);
-            var msdf = new Bitmap<FloatRgb>(2*bitmapWidth, 2*bitmapHeight);
+            int scale = 2;
+            var msdf = new Bitmap<FloatRgb>(scale * bitmapWidth, scale * bitmapHeight);
+
             var generator = Generate.Msdf();
             generator.Output = msdf;
             generator.Range = 3.0;
             generator.EdgeThreshold = 3.0;
-            generator.Scale = new Vector2(2.0);
+            generator.Scale = new Vector2(scale);
 
-            for (int i = 0; i < 5; ++i)
+            for (int i = 0; i < 1; ++i)
             {
                 shape.Normalize();
                 Coloring.EdgeColoringSimple(shape, 3.0);
@@ -32,7 +34,7 @@ namespace Msdfgen.ManualTest
             Bmp.SaveBmp(msdf, "output.bmp");
             {
                 // MDSF Text
-                var rast = new Bitmap<float>(32, 32);
+                var rast = new Bitmap<float>(1024, 1024);
                 Render.RenderSdf(rast, msdf, 6.0);
                 Bmp.SaveBmp(rast, "rasterized.bmp");
             }
--- README.md
+++ README.md
@@ -1,6 +1,31 @@
-<h1 align="center">SharpMSDF</h1>
+﻿<h1 align="center">SharpMSDF</h1>
 .NET port of Chlumsky's msdfgen + msdf-atlas-gen with OpenType loader all in pure C# (no native dlls included) 
 
----
 
-*(WIP)*
+## Features :
+- ✅ No native dependencies (should work on any platform that works with .NET 8.0+).
+- ✅ OpenType (ttf/otf/..) font loader.
+- ✅ Glyph MSDF generator.
+- ⏳ Uses various libraries (MonoGame, KNI and Skia/System.Drawing) for geometry and bitmaping.
+- ⏳ MSDF Atlas generator.
+- ⏳ Dotnet tool for MSDF Atlas generation.
+- ⏳ Better optimized codebase.
+
+## Licenses:
+
+MIT License 2025 FenzDev
+
+MIT License 2014-2025, Viktor Chlumsky
+
+(LayoutFarm/Typography licenses can be seen in header of its source files) 
+
+
+## References:
+
+OpenType: 
+https://github.com/LayoutFarm/Typography/tree/master/Typography.OpenFont
+
+MSDF:
+https://github.com/Chlumsky/msdfgen
+https://github.com/Chlumsky/msdf-atlas-gen
+https://github.com/DWVoid/Msdfgen.Net/blob/master/Msdfgen.IO/ImportFont.cs
